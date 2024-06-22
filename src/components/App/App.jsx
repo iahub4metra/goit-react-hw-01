2788648df9603946feac2f984654e612bd4105ed
iahub4metra <buhai.artem7@gmail.com>
2024-06-22 15:13:21 +0100

    s

---
+++ src/components/App/App.jsx
@@ -0,0 +1,20 @@
+import userData from "../../userData.json";
+import Profile from "../profile/Profile";
+
+const App = () => {
+    return (
+        <>
+            <Profile
+                name={userData.username}
+                tag={userData.tag}
+                location={userData.location}
+                image={userData.avatar}
+                stats={userData.stats}
+            />
+        
+        
+        
+        </>
+    )
+}
+export default App
