9071615c06efe76e725225ea897bddbc2a53bd32
iahub4metra <buhai.artem7@gmail.com>
2024-06-22 19:29:26 +0100

    profile

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,26 +1,28 @@
-const Profile = ({name, tag, location, image, stats}) => {
+import css from "./Profile.module.css"
+const Profile = ({ name, tag, location, image, stats }) => {
     return (
-        <div>
-            <div>
+        <div className={css.profileContainer}>
+            <div className={css.profileContainerImage}>
                 <img
                     src={image}
                     alt="User avatar"
+                    className={css.profileImage}
                 />
-                <p>{name}</p>
-                <p>@{tag}</p>
-                <p>{location}</p>
+                <p className={css.profileTitle}>{name}</p>
+                <p className={css.profileText}>@{tag}</p>
+                <p className={css.profileText}>{location}</p>
             </div>
 
-            <ul>
-                <li>
+            <ul className={css.profileList}>
+                <li className={css.profileListItem}>
                     <span>Followers</span>
                     <span>{stats.followers}</span>
                 </li>
-                <li>
+                <li className={css.profileListItem}>
                     <span>Views</span>
                     <span>{stats.views}</span>
                 </li>
-                <li>
+                <li className={css.profileListItem}>
                     <span>Likes</span>
                     <span>{stats.likes}</span>
                 </li>
